0a87f60de25b1264eeb04b0b02c12b95f237f992
Heinrich Schuchardt <xypron.glpk@gmx.de>
2012-12-21 16:24:48 +0100

    started adding build scripts
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

---
+++ autogen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+test -f configure.ac || {
+  echo "Please, run this script in the top level project directory."
+  DIE=1
+}
+
+libtoolize --force --copy
+aclocal -I m4
+autoconf
+autoheader
+automake --add-missing
+
+echo "For installation instructions, please, refer to file INSTALL."
